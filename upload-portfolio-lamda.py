21354fe1744f3337949b8dfddbaaf3f9c812b19a
cory maklin <corymaklin@gmail.com>
2018-04-28 06:32:25 -0400

    use SNS when portfolio is deployed

--- upload-portfolio-lamda.py
+++ upload-portfolio-lamda.py
@@ -6,19 +6,29 @@ import mimetypes
 
 def lambda_handler(event, context):
     
-    s3 = boto3.resource('s3', config=Config())
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:084696551378:deployPortfolioTopic')
     
-    portfolio_bucket = s3.Bucket('portfolio.coryjmaklin.com')
-    build_bucket = s3.Bucket('portfoliobuild.coryjmaklin.com')
+    try:
+        s3 = boto3.resource('s3', config=Config())
+        
+        portfolio_bucket = s3.Bucket('portfolio.coryjmaklin.com')
+        build_bucket = s3.Bucket('portfoliobuild.coryjmaklin.com')
+        
+        portfolio_zip = BytesIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        
+        print ('Job done!')
+        topic.publish(Subject="Portfolio deployed", Message="Portfolio deployed successfully")
+    except:
+        topic.publish(Subject="Portfolio Deploy failed", Message="The Portfolio deployment failed")
+        raise
     
-    portfolio_zip = BytesIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-    
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-    
-    return 'Hello from Lambda'
+    return "Hello from Lambda"
