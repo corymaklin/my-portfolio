bfeb66a9bd0ae40d9123cd2b20e4329311891218
cory maklin <corymaklin@gmail.com>
2018-04-28 15:52:32 -0400

    Add tests for react componenets

---
+++ __tests__/test-example-work.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ExampleWork from '../js/example-work';
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+const myWork = [
+    {
+        'title': "Work Example",
+        'image': {
+            'desc': "example screenshot of a project involving code",
+            'src': "images/example1.png",
+            'comment': ""
+        }
+    },
+    {
+        'title': "Portfolio Boilerplate",
+        'image': {
+            'desc': "A Serverless Portfolio",
+            'src': "images/example2.png",
+            'comment': ""
+        }
+    }
+];
+
+describe("ExampleWork component", () => {
+    let component = shallow(<ExampleWork work={myWork} />);
+    it("Should be a 'section' element", () => {
+        expect(component.type()).toEqual('section');
+    });
+});
